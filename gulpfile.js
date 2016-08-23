653a0e0beb6b32c99279a8d2687e5505fa10dede
iamrelos <iamrelos@gmail.com>
2016-08-23 13:06:29 -0500

    Changed from Less to Sass

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,22 @@ gulp.task('less-compile', () => {
         .pipe(gulp.dest(config.paths.CSS));
 });
 
-gulp.task('minify-css', ['less-compile'], () => {
+gulp.task('sass-compile', () => {
+    return gulp.src(config.paths.CSS + 'sass/style.scss')
+        .pipe(glp.sass().on('error', glp.sass.logError))
+        .pipe(gulp.dest(config.paths.CSS));
+});
+
+gulp.task('autoprefixer', ['sass-compile'], () =>
+    gulp.src(config.paths.CSS + 'style.css')
+        .pipe(glp.autoprefixer({
+            browsers: ['last 4 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(config.paths.CSS))
+);
+
+gulp.task('minify-css', ['autoprefixer'], () => {
     return gulp.src(config.paths.CSS + 'style.css')
         .pipe(glp.cssnano(config.gulp.cssnanoOpts))
         .pipe(glp.rename('style.min.css'))
