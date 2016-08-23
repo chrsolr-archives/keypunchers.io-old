99cff2e9bd18be9fd986ac2f78fb42572a302d0d
Christian Soler <twenty40@gmail.com>
2016-08-22 22:30:19 -0500

    updated

--- public/js/modules/bootstrapper.js
+++ public/js/modules/bootstrapper.js
@@ -4,16 +4,42 @@ define(["require", "exports", 'jquery'], function (require, exports, $) {
         function Bootstrapper() {
         }
         Bootstrapper.prototype.initialize = function () {
+            this.setHideNavbar();
             this.setFooterHeight();
         };
-        Bootstrapper.prototype.setPageTransition = function () {
-            $('a.transition').on('click', function (e) {
-                e.preventDefault();
-                var url = $(this).attr('href');
-                $('.main-wrapper').fadeOut('fast', function () {
-                    document.location.href = url;
-                });
+        Bootstrapper.prototype.setHideNavbar = function () {
+            var $nav = $('.navbar-default');
+            var lastPosition = 0;
+            var isScrolled = false;
+            var delta = 5;
+            var speed = 250;
+            $(window).scroll(function () {
+                isScrolled = true;
             });
+            function onHasScrolled() {
+                var top = $(window).scrollTop();
+                if (Math.abs(lastPosition - top) <= delta)
+                    return;
+                if ((top > lastPosition) && (top > $nav.outerHeight())) {
+                    $nav.animate({
+                        top: '-' + Number($nav.outerHeight() + 10) + 'px'
+                    }, speed);
+                }
+                else {
+                    if (top + $(window).height() < $(document).height()) {
+                        $nav.animate({
+                            top: '0px'
+                        }, speed);
+                    }
+                }
+                lastPosition = top;
+            }
+            setInterval(function () {
+                if (isScrolled) {
+                    onHasScrolled();
+                    isScrolled = false;
+                }
+            }, 250);
         };
         Bootstrapper.prototype.setFooterHeight = function () {
             var $footer = $('.footer');
--- public/typescript/modules/bootstrapper.ts
+++ public/typescript/modules/bootstrapper.ts
@@ -4,19 +4,51 @@ export class Bootstrapper {
     constructor() { }
 
     initialize() {
+        this.setHideNavbar();
         this.setFooterHeight();
     }
 
-    setPageTransition() {
-        $('a.transition').on('click', function (e: any) {
-            e.preventDefault();
+    setHideNavbar() {
+        var $nav = $('.navbar-default');
+        var lastPosition = 0;
+        var isScrolled = false;
+        var delta = 5;
+        var speed = 250;
 
-            const url = $(this).attr('href');
-
-            $('.main-wrapper').fadeOut('fast', function () {
-                document.location.href = url;
-            });
+        $(window).scroll(function () {
+            isScrolled = true;
         });
+
+        function onHasScrolled() {
+            var top = $(window).scrollTop();
+
+            if (Math.abs(lastPosition - top) <= delta)
+                return;
+
+            if ((top > lastPosition) && (top > $nav.outerHeight())) {
+                $nav.animate({
+                    top: '-' + Number($nav.outerHeight() + 10) + 'px'
+                }, speed);
+            } else {
+                if (top + $(window).height() < $(document).height()) {
+                    $nav.animate({
+                        top: '0px'
+                    }, speed);
+                }
+            }
+
+            lastPosition = top;
+        }
+
+        setInterval(function () {
+
+            if (isScrolled) {
+                onHasScrolled();
+
+                isScrolled = false;
+            }
+
+        }, 250);
     }
 
     setFooterHeight() {
@@ -24,6 +56,6 @@ export class Bootstrapper {
         const $main_wrapper = $('.main-wrapper');
 
         $main_wrapper.css('margin-bottom', $footer.outerHeight());
-        $footer.css({opacity: 1});
+        $footer.css({ opacity: 1 });
     }
 }
