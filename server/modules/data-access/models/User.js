6faeb09357cee5c6e3f065b42d1dd7cfc0ec3924
iamrelos <twenty40@gmail.com>
2016-08-25 21:07:16 -0500

    ...

--- server/modules/data-access/models/User.js
+++ server/modules/data-access/models/User.js
@@ -10,19 +10,12 @@ const UserModel = (() => {
         name: { type: String, trim: true, required: true},
         name_canonical: { type: String, uppercase: true, required: true, trim: true, index: { unique: true }},
         displayName: { type: String, required: true, trim: true },
+        tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'blogs' }],
 
         isAnAdmin: { type: Boolean, default: false },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now },
-<<<<<<< HEAD
 
-=======
-        displayName: { type: String, required: true, trim: true },
-        email: { type: String, required: true, index: { unique: true }, trim: true },
-        tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'blogs' }],
-        firstName: { type: String },
-        lastName: { type: String },
->>>>>>> 4625490edf08cf59a275c7883e5ed3b4073b9524
         google: { type: Object },
         twitter: { type: Object },
         facebook: { type: Object },
