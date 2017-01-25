d7740ce9c8ef6de3fe8a9ee0cf76d4d10d74bd1e
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2017-01-24 21:49:09 -0600

    updated

---
+++ public/js/scripts/blog.js
@@ -0,0 +1,17 @@
+define(["require", "exports", 'jquery', "bootstrap_validator"], function (require, exports, $) {
+    "use strict";
+    var Blog = (function () {
+        function Blog() {
+            $(document).ready(function () {
+                $('form').validator().on('submit', function (e) {
+                    var is_valid = !e.isDefaultPrevented();
+                    if (!is_valid)
+                        return;
+                    $('form').find(':submit').attr('disabled', 'disabled');
+                });
+            });
+        }
+        return Blog;
+    }());
+    return Blog;
+});
