fac4a781d010493f8ac88e031bb3403dc30b7fb1
iamrelos <iamrelos@gmail.com>
2017-05-10 15:38:13 -0500

    ...

--- server/routes/apis/xa-android.js
+++ server/routes/apis/xa-android.js
@@ -3,10 +3,10 @@
 const mountRoutes = (app) => {
     app.get('/api/android/xa/version', (req, res) => {
         return res.json({
-            version_code: 1,
-            version_name: '0.0.2',
+            version_code: 2,
+            version_name: '0.0.3',
             xda_thread_url: 'https://forum.xda-developers.com/android/apps-games/app-xa-unofficial-xboxachievements-com-t3600450',
-            download_url: 'https://forum.xda-developers.com/attachment.php?attachmentid=4141216&d=1494138063'
+            download_url: 'https://firebasestorage.googleapis.com/v0/b/xa-android.appspot.com/o/xa-android-apks%2FXA-0.0.2.apk?alt=media&token=d8447b4e-68ae-48fd-b70a-50c554b8db76'
         });
     });
 };
