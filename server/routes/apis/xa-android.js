d4db4e55c2041f94890cd862355a55a111152b1a
christiansoler <iamrelos@gmail.com>
2017-05-10 21:05:00 -0500

    ....

--- server/routes/apis/xa-android.js
+++ server/routes/apis/xa-android.js
@@ -6,7 +6,7 @@ const mountRoutes = (app) => {
             version_code: 2,
             version_name: '0.0.3',
             xda_thread_url: 'https://forum.xda-developers.com/android/apps-games/app-xa-unofficial-xboxachievements-com-t3600450',
-            download_url: 'https://firebasestorage.googleapis.com/v0/b/xa-android.appspot.com/o/xa-android-apks%2FXA-0.0.2.apk?alt=media&token=d8447b4e-68ae-48fd-b70a-50c554b8db76'
+            download_url: 'https://firebasestorage.googleapis.com/v0/b/xa-android.appspot.com/o/xa-android-apks%2Fxa-0.0.3.apk?alt=media&token=f05049ad-783e-48e2-81bc-4a973438c2ed'
         });
     });
 };
