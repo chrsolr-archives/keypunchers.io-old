cf345492c99efee748d6042af2e5f32a9e7351b6
Christian Soler <iamrelos@gmail.com>
2017-05-30 19:17:01 -0500

    Update xa-android.js

--- server/routes/apis/xa-android.js
+++ server/routes/apis/xa-android.js
@@ -3,8 +3,8 @@
 const mountRoutes = (app) => {
     app.get('/api/android/xa/version', (req, res) => {
         return res.json({
-            version_code: 4,
-            version_name: '0.0.5',
+            version_code: 5,
+            version_name: '0.0.6',
             xda_thread_url: 'https://forum.xda-developers.com/android/apps-games/app-xa-unofficial-xboxachievements-com-t3600450',
             download_url: 'https://labs.xda-developers.com/store/app/io.keypunchers.xa'
         });
